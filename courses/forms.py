06db38fe771e3d6d7f687eb29bd0bbfb8ba04f47
Andrei Degtev <adnezdes@gmail.com>
2025-02-09 15:35:42 +0100

    added basic project

---
+++ courses/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import TopicDocument, CourseTopic
+
+
+class TopicDocumentForm(forms.ModelForm):
+    topic = forms.ModelChoiceField(
+        queryset=CourseTopic.objects.all(),
+        empty_label="Select a Topic",
+        required=True
+    )
+
+    class Meta:
+        model = TopicDocument
+        fields = ['name', 'file', 'topic']
+
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .forms import TopicDocumentForm
+from .models import TopicDocument
+
+
+def document_list(request):
+    if request.method == "POST":
+        form = TopicDocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = TopicDocumentForm()
+
+    documents = TopicDocument.objects.all()
+    return render(request, template_name='document_list.html', context={'form': form, 'documents': documents})
+
+
+def update_document(request, document_id):
+    document = get_object_or_404(TopicDocument, id=document_id)
+
+    if request.method == "POST":
+        form = TopicDocumentForm(request.POST, request.FILES, instance=document)
+        if form.is_valid():
+            form.save()
+            return redirect('document_list')
+    else:
+        form = TopicDocumentForm(instance=document)
+
+    return render(request, template_name='update_document.html', context={'form': form, 'document': document})
+
+
+def delete_document(request, document_id):
+    document = get_object_or_404(TopicDocument, id=document_id)
+
+    if request.method == 'POST':
+        # delete file itself
+        if document.file:
+            document.file.delete(save=False)
+
+        # delete db row
+        document.delete()
+
+    return render(request, template_name='confirm_delete.html', context={'document': document})
+
+   
+
+
+
